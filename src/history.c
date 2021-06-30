db2a4a5366712795136a6df151f79bb3c6d130be
Jabaca3 <jojobaca123@gmail.com>
2021-06-29 20:18:03 -0600

    History working

--- src/history.c
+++ src/history.c
@@ -5,43 +5,48 @@
 
 List *init_history(){
   List *user_history = (List*) malloc(sizeof(Item)*100);
+  return user_history;
 }
 
 void add_history(List *list, char *str){
   Item *node = list->root;
-  int id = 1;
   
   //If we are barly starting history
-  if (node == NULL) {
-    list->root = (Item*) malloc(sizeof (Item));
-    list->root->id = id;
-    list->root->str = str;
-    list->root->next = NULL;
-  }
-  
-  //If history is already started
-  else {
-    while(node->next != NULL) {
-      node = node->next;
-      id++;
+    if (node == NULL) {
+      node = (Item*) malloc(sizeof (Item));
+      node->id = 1;
+      node->str = str;
+      node->next = NULL;
+      list->root = node;
+      printf("%s",node->str);
+      printf("%i\n",node->id);
     }
-    node->next = (Item*) malloc(sizeof (Item));
-    node->next->id = id++;
-    node->next->str = str;
-    node->next->next = NULL;
+    
+    else {
+      int id=2;
+      while(node->next != NULL) {
+	node = node->next;
+	id++;
+      }
+      node->next = (Item*) malloc(sizeof (Item));
+      node->next->id = id;
+      node->next->str = str;
+      node->next->next = NULL;
+      printf("%s\n",node->next->str);
+      printf("%i\n",node->next->id);
+    }
   }
-}
 
 char *get_history(List *list, int id){
   Item *node = list->root;
 
-  while(node != NULL){
+  while( node != NULL){
     if(node->id == id){
       return node->str;
     }
     node = node->next;
   }
-  return "YOUR STRING NOT FOUND :(";
+  return "Error";
 }
 
 void print_history(List *list){
--- src/uimain.c
+++ src/uimain.c
@@ -4,34 +4,36 @@
 #include "history.h"
 
 int main(){
+  List *history = init_history();
   while(1){
     
-  printf("\n$ ");
-  char user_input[100];
-  List *history = init_history();
-  fgets(user_input, 100, stdin);
-  printf("%s\n", user_input);
+    printf("\n$ ");
+    char user_input[100];
+    fgets(user_input, 100, stdin);
+    printf("%s\n", user_input);
 
-  //Checking user input for history request
-  if(user_input[0] == 33 && (user_input[1] > 48 && user_input[1] < 58)){
+    //Checking user input for history request
+    // 33 = ! ascii(48-58) = numbers(1-9)
+    if(user_input[0] == 33 && (user_input[1] > 47 && user_input[1] < 58)){
     
-    //Recalling history
-    printf("%s\n", "--------recalling history!------------");
-    int num = atoi(user_input+1);
-    char* token = get_history(history, num);
-    printf("%s%s\n","$ ", token);
-
-    //Printing tokens from input
-    char** tokenized = tokenize(token);
-    print_tokens(tokenized);
+      printf("%s\n", "--------recalling history!------------");
+      int num = atoi(user_input+1);
+      char* token = get_history(history, num);
+      if(*token){
+	printf("%s%s\n","$ ", token);
+    }
+    else{
+      printf("%s\n", "Your token does not exist");
+    }
   }
   
   else{
     char **tokens= tokenize(user_input);
     print_tokens(tokens);
     printf("%s\n", "---------adding history!---------------");
-    char* copy = copy_str(user_input, str_length(user_input));
-    add_history(history, copy); //works
+    char *copy = copy_str(user_input, str_length(user_input));
+    printf("%s\n", copy);
+    add_history(history, copy);
     }
   }
 }
